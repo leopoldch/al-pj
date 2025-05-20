047032eb4b9c3480ceb590056c42d31ef4c8bea6
leo <leopold@lchappuis.fr>
2025-05-20 23:08:36 +0200

    [fix containers]

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,10 +1,14 @@
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
+
+django.setup()
+
 from core.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
