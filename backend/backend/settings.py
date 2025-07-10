df91ba2e63c9f4e06f55f40131d44753ea49e911
leo <leopold@lchappuis.fr>
2025-07-10 20:57:08 +0200

    [misc] tryfix static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -165,7 +165,7 @@ SIMPLE_JWT = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "dj/static/"
+STATIC_URL = "/dj/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
 
