392aba6e8c5f2a532cc53a3f87260f9caeca738c
leo <leopold@lchappuis.fr>
2025-07-27 15:39:43 +0200

    minor fix

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "first_name", "last_name", "email", "date_joined"]
+        fields = ["id", "username", "email", "date_joined"]
 
 
 class MessageSerializer(serializers.ModelSerializer):
